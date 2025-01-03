e6d001efddba60ad4e6172d83837a81bb0ca8358
kamlesh sah <Sahk0292@gmail.com>
2025-01-03 23:17:03 +0545

    Add VariableIntro class with variable declarations

---
+++ taskDayOne.java
@@ -0,0 +1,22 @@
+// Make  a new variable for your
+/*
+ * name 
+ * gender
+ * age
+ * over18 boolean
+ * Initializa the above variable
+ * make 2 new variable int
+ * fatherAge, motherAge -> declare and initialize in the same line
+ */
+
+public class taskDayOne {
+  public static void main(String[] args) {
+    String name = "Kamlesh sah";
+    char gender = 'M';
+    int age = 21;
+    boolean over18 = true;
+    int fatherAge = 50, motherAge = 45;
+    System.out.println("Name: " + name + "\nGender: " + gender + "\nAge: " + age + "\nOver 18: " + over18 + "\nFather Age: " + fatherAge + "\nMother Age: " + motherAge);
+
+  }
+}
