40a972d2630a3e3ba86ebf4e06b11577d642e8f4
kamlesh sah <Sahk0292@gmail.com>
2025-01-10 21:33:17 +0545

    Variables In Java

---
+++ Basic/02_VariableIntro.java
@@ -0,0 +1,26 @@
+package Basic;
+public class VariableIntro {
+	public static void main(String[] args) {
+		boolean booleanVariable = true;
+		// Multiple declarations in one line
+		int intVariable, intVariable2;
+		intVariable = 5;
+
+
+		// Primitive Data Types 
+		// 8 bits = 1 byte 
+		byte byteVariable = 127; // 8 bits
+		short shortVariable = 32767; // 16 bits
+		int intVariableForPri = 2147483647; // 32 bits
+		long longVariable = 9223372036854775807L; // 64 bits
+
+
+
+		// non primitive data types
+		String stringVariable = "Hello World"; // String is enclosed in double quotes
+
+		// using class
+		String stringVariable2 = new String("Hello World");	// original is a class, new is a keyword
+
+	}
+}
